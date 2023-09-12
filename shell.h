ee5a9ab20cf3411030c15036f76353faae7a6e2a
elmouftakir <abderahim.mouftakir@gmail.com>
2023-09-12 14:01:17 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <errno.h>
+extern char **environ;
+#endif
