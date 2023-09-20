c71846f1f721e5583a1ac84277aa30c136317b1a
elmouftakir <abderahim.mouftakir@gmail.com>
2023-09-20 05:39:15 -0700

    getenv

---
+++ getenv_shell.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * _getenv - get variables enviroment
+ * @variable: parameter in
+ * Return: command
+ */
+char *_getenv(char *variable)
+{
+	int i;
+	char *tmp;
+	char *key;
+	char *value;
+	char *_env;
+
+	for (i = 0; environ[i]; i++)
+	{
+		tmp = _strdup(environ[i]);
+		key = strtok(tmp, "=");
+		if (_strcmp(key, variable) == 0)
+		{
+			value = strtok(NULL, "\n");
+			_env = _strdup(value);
+			return (_env);
+		}
+	free(tmp), tmp = NULL;
+	}
+	return (NULL);
+}
