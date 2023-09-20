56e0892f10e82d7f572f644179629ea55033da1d
elmouftakir <abderahim.mouftakir@gmail.com>
2023-09-20 05:37:34 -0700

    header

--- shell.h
+++ shell.h
@@ -1,15 +1,38 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <sys/types.h>
-#include <stddef.h>
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <signal.h>
+
 extern char **environ;
+void sig_handler(int);
+char *val_getline(void);
+int _memory(char *buff);
+int _memory1(char *buff);
+char **fun_strtok(char *buff, int pointer);
+void exit1(char *buffer, char **commands);
+int env1(char *buff, char **commands);
+char *_getenv(char *variable);
+char *_strcpy(char *dest, char *src);
+char *_strdup(char *str);
+char *_str_str(char *haystack, char *needle);
+int _strcmp(char *s0, char *s1);
+char *get_path(char *command);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *f_function(char **folder, char *command);
+void fun_fork(char *buffer, char **command, char **argv, int count);
+char *_itoa(int number);
+int *_perror(char *argv, char *str, char *command);
+
+
 #endif
+
